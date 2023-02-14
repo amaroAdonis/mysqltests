743685e389024cb2d62f1988803b52fa50b3bc7b
Amaro Adonis Moraes Amorim <amaro.adonis@gmail.com>
2023-02-14 18:48:36 -0300

    Projeto versão aula 273(deleting data) JDBC - Nélio Alves

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 
 import db.DB;
+import db.DbIntegrityException;
 
 import java.sql.*;
 import java.text.ParseException;
@@ -14,17 +15,15 @@ public class Program {
         PreparedStatement st = null;
         try {
             conn = DB.getConnection();
-            st = conn.prepareStatement(
-                    "update seller set BaseSalary = BaseSalary + ? where (DepartmentId = ?)");
-            st.setDouble(1,200.00);
-            st.setInt(2,2);
+            st = conn.prepareStatement("delete from department where id = ? ");
+            st.setInt(1,6);
 
             int rowsAffected = st.executeUpdate();
 
             System.out.println("Done! Rows affected: " + rowsAffected);
         }
         catch (SQLException e){
-            e.printStackTrace();
+            throw new DbIntegrityException(e.getMessage());
         }
         finally {
             DB.closeStatement(st);
